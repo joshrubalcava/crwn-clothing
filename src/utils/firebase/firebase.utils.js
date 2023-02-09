721b2eaf66e8846308b235c5fe38d8ed8692d893
Josh Rubalcava <josh.rubalcava@yahoo.com>
2023-02-08 19:49:30 -0800

    added our sign in component as well as firebase utils and brought in firebase library

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,68 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA3WEpb6tytA-Hh7NiUN7OyOvFXO0hicI",
+  authDomain: "crwn-clothing-database-aff34.firebaseapp.com",
+  projectId: "crwn-clothing-database-aff34",
+  storageBucket: "crwn-clothing-database-aff34.appspot.com",
+  messagingSenderId: "531373771724",
+  appId: "1:531373771724:web:75dd01fd3b858172b56b69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  // if user data does not exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    // create / set the document with the data from userAuth in my collection
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (err) {
+      console.log('error creating the user', err.message);
+    }
+  }
+
+  // if user data exists, return userDocRef
+  return userDocRef;
+}
